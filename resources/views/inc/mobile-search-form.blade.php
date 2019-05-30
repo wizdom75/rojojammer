75b3c024a0864b972de7a32b5f4a80990203e07f
Peter Ncube <pmn@outlook.com>
2019-05-30 10:42:53 +0100

    Search form on mobile with padding

--- resources/views/inc/mobile-search-form.blade.php
+++ resources/views/inc/mobile-search-form.blade.php
@@ -1,5 +1,5 @@
 <div class="mobile-search">
-    <ul class="col w-100 mb-0">
+    <ul class="col w-100">
         <form method="GET" action="/results" accept-charset="UTF-8">
             <div class="input-group">
                 <input id="sq" class="form-control form-control-lg  rounded-0" placeholder="Search" aria-label="search" aria-describedby="search-form" name="q" type="text" value="{{Request::get('q')}}">
